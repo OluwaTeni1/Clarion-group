42ddbb87772c57052f34c78430a3803df185c2c6
OluwaTeni1 <isolateni02@gmail.com>
2024-08-18 20:34:19 +0100

    Updated

---
+++ script.js
@@ -0,0 +1,106 @@
+<<<<<<< HEAD
+let slide = document.querySelectorAll('.slide');
+                var current = 0;
+
+                function cls() {
+                    for (let i = 0; i < slide.length; i++) {
+                        slide[i].style.display = 'none';
+                    }
+                }
+
+                function next(){
+                    cls();
+                    if(current === slide.length-1) current = -1;
+                    current++;
+
+                    slide[current].style.display = 'block';
+                    slide[current].style.opacity = 0.4;
+
+                    var x = 0.4;
+                    var intX = setInterval(function(){
+                        x+=0.1;
+                        slide[current].style.opacity = x;
+                        if (x >= 1) {
+                            clearInterval(intX);
+                            x = 0.4;
+                        }
+                    }, 100)
+                }
+
+                function prev() {
+                        cls();
+                        if (current === 0) current = slide.length;
+                        current--;
+
+                        slide[current].style.display = 'block';
+                        slide[current].style.opacity = 0.4;
+
+                        var x = 0.4;
+                        var intX = setInterval(function () {
+                            x += 0.1;
+                            slide[current].style.opacity = x;
+                            if (x >= 1) {
+                                clearInterval(intX);
+                                x = 0.4;
+                            }
+                        }, 100)
+                    }
+
+                function start() {
+                    cls();
+                    slide[current].style.display = 'block';
+                }
+=======
+let slide = document.querySelectorAll('.slide');
+                var current = 0;
+
+                function cls() {
+                    for (let i = 0; i < slide.length; i++) {
+                        slide[i].style.display = 'none';
+                    }
+                }
+
+                function next(){
+                    cls();
+                    if(current === slide.length-1) current = -1;
+                    current++;
+
+                    slide[current].style.display = 'block';
+                    slide[current].style.opacity = 0.4;
+
+                    var x = 0.4;
+                    var intX = setInterval(function(){
+                        x+=0.1;
+                        slide[current].style.opacity = x;
+                        if (x >= 1) {
+                            clearInterval(intX);
+                            x = 0.4;
+                        }
+                    }, 100)
+                }
+
+                function prev() {
+                        cls();
+                        if (current === 0) current = slide.length;
+                        current--;
+
+                        slide[current].style.display = 'block';
+                        slide[current].style.opacity = 0.4;
+
+                        var x = 0.4;
+                        var intX = setInterval(function () {
+                            x += 0.1;
+                            slide[current].style.opacity = x;
+                            if (x >= 1) {
+                                clearInterval(intX);
+                                x = 0.4;
+                            }
+                        }, 100)
+                    }
+
+                function start() {
+                    cls();
+                    slide[current].style.display = 'block';
+                }
+>>>>>>> 168b9477ab30d584872f413df740372882686cdd
+                start();
